81f940797cf69ee9c3eb38ccf9ef616b174a1fda
Maximushek1 <max.grenko@gmail.com>
2021-01-14 21:28:28 +0300

    Implement the next stage of algorithms * add score counter

--- main.py
+++ main.py
@@ -23,6 +23,16 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space difficulties")
 clock = pygame.time.Clock()
 
+font_name = pygame.font.match_font('arial')
+
+
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -130,6 +140,7 @@ for i in range(8):
     m = Mob()
     all_sprites.add(m)
     mobs.add(m)
+score = 0
 
 # цикл игры
 running = True
@@ -147,8 +158,10 @@ while running:
     # обновление
     all_sprites.update()
 
+    # проверка, попала ли пуля в моб
     hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
     for hit in hits:
+        score += 50 - hit.radius
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
@@ -162,6 +175,7 @@ while running:
     screen.fill(BLACK)
     screen.blit(background, background_rect)
     all_sprites.draw(screen)
+    draw_text(screen, str(score), 18, WIDTH / 2, 10)
     # после отрисовки всего, переворачиваем экран
     pygame.display.flip()
 
