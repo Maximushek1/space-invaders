6e0c6a8912c02cb12804e6ae980661f168991170
Maximushek1 <max.grenko@gmail.com>
2021-01-14 17:18:18 +0300

    Set the radius for the mob sprite

--- main.py
+++ main.py
@@ -59,6 +59,8 @@ class Mob(pygame.sprite.Sprite):
         self.image = meteor_img
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width * .85 / 2)
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, -40)
         self.speedy = random.randrange(1, 8)
@@ -130,7 +132,7 @@ while running:
         mobs.add(m)
 
     # проверка, не ударил ли моб игрока
-    hits = pygame.sprite.spritecollide(player, mobs, False)
+    hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
     if hits:
         running = False
 
